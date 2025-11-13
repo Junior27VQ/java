b70f149b9d4687b97b6e3b80ebe96263380486ce
Junior27VQ <favq1999@outlook.com>
2025-11-12 19:09:07 -0500

    invocacion de funciones en la clase TestCalculadora exitoso

---
+++ clases/src/com/krakedev/TestCalculadora.java
@@ -0,0 +1,28 @@
+package com.krakedev;
+
+public class TestCalculadora {
+
+	public static void main(String[] args) {
+		Calculadora calc=new Calculadora();
+		int resultadoSuma;
+		int resultadoResta;
+		double resultadoMultiplicar;
+		double resultadoDividir;
+		double resultadoPromedio;
+		
+		resultadoSuma=calc.sumar(5, 3);
+		resultadoResta=calc.restar(10, 3);
+		resultadoMultiplicar=calc.multiplicar(25.40, 5.20);
+		resultadoDividir=calc.dividir(125.00, 2.00);
+		resultadoPromedio=calc.promedio(10, 07, 05);
+		calc.mostrarResultado();
+		
+		System.out.println("RESULTADO SUMA: "+resultadoSuma);
+		System.out.println("RESULTADO RESTA: "+resultadoResta);
+		System.out.println("RESULTADO MULTIPLICAR: "+resultadoMultiplicar);
+		System.out.println("RESULTADO DIVIDIR: "+resultadoDividir);
+		System.out.println("RESULTADO PROMEDIO: "+resultadoPromedio);
+
+	}
+
+}
